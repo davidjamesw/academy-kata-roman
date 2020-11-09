f441b4a89631ad5b48d148f26c4bb5dde765c262
davidjamesw <davidjwright8@gmail.com>
2020-11-09 22:14:41 +0000

    Reduce if next number lower than previous
    Next commit will validate next number is valid reducer

--- romanNumerals.go
+++ romanNumerals.go
@@ -1,6 +1,8 @@
 package romanNumerals
 
-import "strings"
+import (
+	"strings"
+)
 
 var romanArabic = map[string]int{
 	"I": 1,
@@ -15,10 +17,19 @@ var romanArabic = map[string]int{
 func convertRomanNumeralToArabic(romanNumerals string) int {
 	splitRomans := strings.Split(romanNumerals, "")
 	var total = 0
-
+	var arabicPrevious = 0
 	for i := len(splitRomans) - 1; i >= 0; i-- {
-		total += romanArabic[splitRomans[i]]
+		arabicCurrent := romanArabic[splitRomans[i]]
+		total += nextNumber(arabicCurrent, arabicPrevious)
+		arabicPrevious = arabicCurrent
 	}
-
 	return total
+}
+
+func nextNumber(arabicCurrent, arabicPrevious int) int {
+	if arabicCurrent < arabicPrevious {
+		return 0 - arabicCurrent
+	} else {
+		return arabicCurrent
+	}
 }
--- romanNumerals_test.go
+++ romanNumerals_test.go
@@ -12,6 +12,10 @@ func TestTwo(t *testing.T) {
 	testMatchesExpected("II", 2, t)
 }
 
+func TestFour(t *testing.T) {
+	testMatchesExpected("IV", 4, t)
+}
+
 func TestFive(t *testing.T) {
 	testMatchesExpected("V", 5, t)
 }
@@ -20,10 +24,16 @@ func TestSix(t *testing.T) {
 	testMatchesExpected("VI", 6, t)
 }
 
+func TestNine(t *testing.T) {
+	testMatchesExpected("IX", 9, t)
+}
+
 func TestFifty(t *testing.T) {
 	testMatchesExpected("L", 50, t)
 }
-
+func TestNineHundred(t *testing.T) {
+	testMatchesExpected("CM", 900, t)
+}
 func TestOneThousand(t *testing.T) {
 	testMatchesExpected("M", 1000, t)
 }
